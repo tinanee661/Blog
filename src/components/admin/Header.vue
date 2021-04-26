312131560abde5ac13c50b2a70903f222a7d12b3
Valentina Neziri <v.neziri@beo-kosovo.com>
2021-04-26 10:47:19 +0200

    fix header style

--- src/components/admin/Header.vue
+++ src/components/admin/Header.vue
@@ -4,27 +4,27 @@
             
                 <div class="collapse navbar-collapse" id="main-navigation">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li class="nav-item" >
+                      <li class="nav-item" id="navadmin">
                             <router-link class="nav-link" to="/admin" >
                             <a>Home</a>
                             </router-link>
                         </li>
-                        <li class="nav-item" >
+                        <li class="nav-item" id="navadmin">
                             <router-link class="nav-link" to="/admin/posts/addpost" >
                             <a>Add Post</a>
                             </router-link>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item" id="navadmin">
                             <router-link class="nav-link" to="/admin/posts/editpost" >
                             <a>Edit Post</a>
                             </router-link>
                         </li>
-                        <li class="nav-item" >
+                        <li class="nav-item" id="navadmin">
                             <router-link class="nav-link" to="/admin/posts/removepost" >
                             <a>Remove Post</a>
                             </router-link>
                         </li>
-                        <li class="nav-item" >
+                        <li class="nav-item" id="navadmin">
                             <router-link class="nav-link" to="/" >
                             <a>Remove User</a>
                             </router-link>
@@ -34,3 +34,26 @@
             </div>
         </nav>
 </template>
+
+
+<style lang="css" scoped>
+    .nav-link {
+        color: black;
+        font-size: 20px;
+        margin-right: 0 !important;
+        padding: 0px 25px !important;
+    }
+
+    .nav-link a::after {
+        content: '';
+        width: 0px;
+        height: 1px;
+        display: block;
+        background: black;
+        transition: 300ms;
+    }
+
+    .nav-link a:hover::after {
+        width: 100%;
+    }
+</style>
