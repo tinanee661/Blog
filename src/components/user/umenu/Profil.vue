2d2d28d347a2b2ceb1c7d0f2c6bddab72a8b1de3
LoretaZeka <68351806+LoretaZeka@users.noreply.github.com>
2021-05-11 13:14:25 +0200

    Skincare/Makeup

---
+++ src/components/user/umenu/Profil.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <UserProfileHeader
+            :user="user"
+            :auth-user="authUser"
+        />
+        <div class="ui stackable grid container">
+            <div class="four wide column">
+                <UserProfileSidebarDetails :user="user"/>
+            </div>
+            <div class="eight wide column">
+                <div class="ui segment">
+                    <h2 class="ui medium dividing header">Tweets</h2>
+
+                    <Tweets
+                        :tweets.sync="tweets"
+                        :auth-user="authUser"
+                    />
+                </div>
+            </div>
+            <div class="four wide column">
+                <WhoToFollow v-if="isLoggedIn"/>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/user/umenu/Skincare.vue
@@ -0,0 +1,52 @@
+<template>
+       <div class="main-container">
+             <div class="container">
+            <div class="title">
+                <h1> Skin Care</h1>
+
+            </div>
+        </div>
+         <div class="gallery">
+                <div class="square-container">
+                    <div class="side-part">
+                        <a
+                            href="https://www.amazon.com/Clinique-Perfecting-Foundation-Concealer-Chamois/dp/B00T6OLHBM/ref=sr_1_3?dchild=1&keywords=foundation&qid=1595624054&sr=8-3">
+                            <div class="square"><img src="@/components/user/img/skincaretest.png" width="200px" height="200"
+                                    alt="3.png">
+                            </div>
+                        </a>
+                        <a
+                            href="https://www.amazon.com/MILANI-Hypnotic-Lights-Powder-Highlighter/dp/B07BWM58KY/ref=sr_1_3?crid=XTUNK001LQ76&dchild=1&keywords=highlighter+makeup&qid=1595624097&sprefix=highlighter+m%2Caps%2C263&sr=8-3">
+                            <div class="square"><img src="@/components/user/img/skincare1.png" width="200px" alt="3.png"></div>
+                        </a>
+                    </div>
+                     <div class="side-part">
+                        <a
+                            href="https://www.amazon.com/NYX-PROFESSIONAL-MAKEUP-Matte-Cannes/dp/B00IAKBG82/ref=sxin_9?ascsubtag=amzn1.osa.c30b8467-568d-48aa-86d8-7ee7f98273ce.ATVPDKIKX0DER.en_US&creativeASIN=B00IAKBG82&crid=RWM6PTH59647&cv_ct_cx=lipstick&cv_ct_id=amzn1.osa.c30b8467-568d-48aa-86d8-7ee7f98273ce.ATVPDKIKX0DER.en_US&cv_ct_pg=search&cv_ct_wn=osp-single-source&dchild=1&keywords=lipstick&linkCode=oas&pd_rd_i=B00IAKBG82&pd_rd_r=1ea31873-9712-410a-af9f-d0c1da396e99&pd_rd_w=dPbWm&pd_rd_wg=RWhnU&pf_rd_p=76540515-1aa7-4007-99bf-d21bc7d2acc8&pf_rd_r=ZRBBS19AKJ8WH6T2WMTG&qid=1595623552&sprefix=lipsti%2Caps%2C266&sr=1-2-72d6bf18-a4db-4490-a794-9cd9552ac58d&tag=totalbeauty-tca-20">
+                            <div class="square"><img src="@/components/user/img/skinproduct1.jpg" width="200px" alt="1.png">
+                            </div>
+                        </a>
+
+                        <a
+                            href="https://www.amazon.com/Paris-Hydrating-580-Peony-Pink/dp/B004BCVC6Y/ref=sr_1_5?dchild=1&keywords=lipstick&qid=1595623691&sr=8-5">
+                            <div class="square"><img src="@/components/user/img/skinproduct2.jpg" width="200px" alt="2.png">
+                            </div>
+                        </a>
+                        <a
+                            href="https://www.amazon.com/Illuminations-773602002801-MAC-frost-lipstick/dp/B0006LNJZ6/ref=sr_1_48?dchild=1&keywords=lipstick&qid=1595623766&sr=8-48">
+                            <div class="square"><img src="@/components/user/img/skinproduct3.jpg" width="200px" height="200"
+                                    alt="3.png">
+                            </div>
+                        </a>
+                        <a
+                            href="https://www.amazon.com/Milani-Statement-Lipstick-Pretty-Natural/dp/B00KQODZUM/ref=sr_1_12?dchild=1&keywords=lipstick&qid=1595623838&sr=8-12">
+                            <div class="square"><img src="@/components/user/img/skinproduct4.jpg" width="200px" alt="3.png">
+                            </div>
+                        </a>
+                </div>
+                    <button onclick="" id="sbutton" type="button">Makeup Products</button>
+                </div>        
+   
+   </div>
+  </div>
+</template>
