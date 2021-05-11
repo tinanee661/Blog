432dae94f97174c97e0202c507f4793ad3485046
elmedina <eb45524@ubt-uni.net>
2021-05-11 12:04:29 +0200

    products

--- src/components/user/umenu/Products.vue
+++ src/components/user/umenu/Products.vue
@@ -1,44 +1,32 @@
 <template>
        
-  <div class="content">
-      <h1>Want to shop the products we use? Click the category you are interested in</h1>
+    <div class="main-container">
+
+  
+
+        <div class="container">
+            <div class="title">
+                <h3> Want to shop the products we use? Click the category you are interested in</h3> 
+                
+            </div>
+
+        </div>
+            
+            <div class="content">
                 <div class="skincare">
-                    <a href="">
-                        <img src="@/components/user/img/skincare2.png" title="insert title" width="333px" alt="insert description here"></a>
+                    <a href="skincare.php">
+                        <img src="@/components/user/img/skincare2.png" title="insert title" width="333px" alt=""></a>
                                
                     </div>
                    
                 <div class="makeup">
-                    <a href="">
-                        <img src="@/components/user/img/makeup2.png" title="insert title" width="333px" alt="insert description here"></a>
+                    <a href="makeup.php">
+                        <img src="@/components/user/img/makeup2.png" title="insert title" width="333px" alt=""></a>
                       
                     </div>
                     
             </div>
-              
+    </div>          
 
 </template>
 
-<style lang="css" scoped>
-
-    .nav-link {
-        background-color: rgba(182, 134, 142, 0.979);
-        color: black;
-        font-size: 20px;
-        margin-right: 0 !important;
-        padding: 0px 25px !important;
-    }
-
-    .nav-link a::after {
-        content: '';
-        width: 0px;
-        height: 1px;
-        display: block;
-        background: black;
-        transition: 300ms;
-    }
-
-    .nav-link a:hover::after {
-        width: 100%;
-    }
-</style>
