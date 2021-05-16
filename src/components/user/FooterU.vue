c1357256d86a319476c09daa330a8dc529f1c599
Nderona <nk44801@ubt-uni.net>
2021-05-16 15:27:13 +0200

    update footer
    footer for user edited

--- src/components/user/FooterU.vue
+++ src/components/user/FooterU.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1> hi</h1>
+
+    <div id="footer-menu">
+<ul>
+<li><a href="src\components\user\umenu\Blog.vue">Home</a></li>
+        <li><a href="src\components\user\umenu\Products.vue">Products</a></li>
+        <li><a href="src\components\user\umenu\Testimonials.vue">Testimonials</a></li>
+        <li><a href="src\components\user\umenu\Contactu.vue">Contact</a></li>
+        <li><a href="src\components\user\umenu\Profil.vue">Profile</a></li>
+        <li><a href="">log out</a></li>
+      
+</ul>
+</div>
+
 </template>
+
+<style lang="css">
+</style>
